b3a2b63430e5db3774e3cc621546b3336d830fc0
mohamed khan <mohamedkhan@mohameds-MBP.lan>
2023-09-25 01:36:13 -0400

    hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Akeels &nbsp;
-            <span className="sm:block hidden text-green-400"> | Portal</span>
+            <span className="sm:block hidden text-purple-400"> | Portal</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
