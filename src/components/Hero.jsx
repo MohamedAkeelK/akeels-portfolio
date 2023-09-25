f96978cefb532c1887831a7e28462ec6b9148a41
mohamed khan <mohamedkhan@mohameds-MBP.lan>
2023-09-25 01:40:59 -0400

    hero section typography

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,14 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText}`}>
             Hi, I'm <span className="text-[#915eff]">Akeel</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3d visuals, user <br className="sm:block hidden" />{" "}
+            interfaces and web applications
+          </p>
         </div>
       </div>
+
+      <ComputersCanvas />
     </section>
   );
 };
